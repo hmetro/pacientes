e5cbc54c1087deae39e084c94a919e529f994add
martinxapps <52510496+martinxapps@users.noreply.github.com>
2022-12-07 14:53:01 -0500

    dev.hm.07.12.22

--- src/views/layout/header-public.js
+++ src/views/layout/header-public.js
@@ -1,36 +1,22 @@
 class HeadPublic {
-
     mom;
     date;
     watch;
-
     constructor() {
         this.mom = moment();
     }
-
     set _setTime({}) {
-
         this.watch = this.mom.format('LT');
-
-
-
     }
-
     get _getTime() {
         return this.watch;
     }
-
     oninit() {
         this.mom.locale('es');
         var _t = this.mom.format('LT');
         if (_t !== this.watch) {
             this.watch = this.mom.format('dddd, DD-MM-YYYY');
-
-
         }
-
-
-
     }
 
     view() {
@@ -46,10 +32,7 @@ class HeadPublic {
                 ]),
                 m("nav.navbar.bg-white.s-dp-1-3.navbar-sticky.type-3.navbar-expand-lg.m-navbar.bcbd_navbar",
                     m("div.container.position-relative", [
-                        m("a.navbar-brand[href='/']",
-                            m("img[src='assets/logo.metrovirtual.png'][alt='Metrovirtual'][width='200rem']")
-                        ),
-
+                        m("img[src='assets/logo.metrovirtual.png'][alt='Metrovirtual'][width='200rem']"),
                         m(".collapse.navbar-collapse.bcbd_collpase_nav[id='navbarSupportedContent']", [
                             m("div.p-3.nav_outer.mr-auto.ml-lg-auto.mr-lg-0", [
                                 m("img.d-block.d-md-none[src='assets/images/logo-white.png'][alt='']"),
@@ -62,7 +45,7 @@ class HeadPublic {
 
                             ]),
                             m("div.navigation_right_area.position-relative.d-none.d-lg-block.navbar-search.my-lg-0",
-                                m("a.btn.fadeInDown-slide.animated.no-border.bg-transparent.medim-btn.grad-bg--3.position-relative.solid-btn.mt-0.text-medium.radius-pill.text-active.text-uppercase.text-white[href='/']",
+                                m("btn.fadeInDown-slide.animated.no-border.bg-transparent.medim-btn.grad-bg--3.position-relative.solid-btn.mt-0.text-medium.radius-pill.text-active.text-uppercase.text-white[href='/']",
                                     "Pacientes"
                                 )
                             )
